d90acee5f59ef335371bb92625efacba353a836e
Ahmad Almustapha <ahmadalmustapha4@gmail.com>
2023-06-01 11:43:43 +0100

    exercise 1.03

---
+++ exercise 1.03/main.py
@@ -0,0 +1,13 @@
+import random
+import string
+import time
+
+
+while True:
+    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+    timestamp = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime())
+    print(f'{timestamp} {random_string}')
+    time.sleep(5)
+    
+    
+    
